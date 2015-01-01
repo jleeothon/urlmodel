7bde58977f51b51e677f08a43dac7cdf9b110991
jleeothon <jleeothon@gmail.com>
2015-01-01 11:44:01 -0500

    Changed to a new version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-urlmodel',
-    version='0.12',
+    version='0.13',
     packages=['urlmodel'],
     include_package_data=True,
     license='MIT',
